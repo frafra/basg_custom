2a88721b05f5091d205a0d5b2a78449102c95c00
Francesco Frassinelli <francesco.frassinelli@nina.no>
2024-11-08 16:57:56 +0100

    Use nix/distrobox/toolbx to run apps without dealing with docker CLI

--- docker_helpers.sh
+++ docker_helpers.sh
@@ -11,34 +11,3 @@ function docker_helpers_kill_by_name() {
 # https://docs.docker.com/engine/security/protect-access/
 #PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}($(docker context show))\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
-
-# example: docker_helpers_export_commands ghcr.io/osgeo/gdal
-function docker_helpers_export_commands() {
-    image="$1"
-    (
-    set -euo pipefail
-    docker pull "$image"
-    mapfile -t commands < <(combine \
-        <(docker run --rm --entrypoint="" "$image" /bin/bash -c 'compgen -c' | sort -u) \
-        not <(compgen -c | sort -u))
-    printf "%s\n" "${commands[@]}" | fzf -m |
-    while read command
-    do
-        wrapper="$HOME/bin/$command"
-        if [ -f "$wrapper" ]
-        then
-            echo "Cannot override $wrapper"
-            continue
-        fi
-cat << EOF > "$wrapper"
-#!/bin/bash
-exec docker run --rm --pid=host \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=\$DISPLAY \
-    -v "\$PWD":/host --workdir /host \
-    -v /tmp:/tmp \
-    --entrypoint="" "$image" "$command" "\$@"
-EOF
-    chmod +x "$wrapper"
-    done
-    )
-}
